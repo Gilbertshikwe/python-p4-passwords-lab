24a1c44434990195c03aaf3ccb2fa41b2cbc65b1
Gilbert <shikweg@gmail.com>
2024-04-12 23:27:42 +0300

    Completed assignment

--- server/app.py
+++ server/app.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-from flask import request, session
-from flask_restful import Resource
+from flask import request, session, jsonify #type:ignore
+from flask_restful import Resource #type:ignore
+from werkzeug.security import generate_password_hash, check_password_hash #type:ignore
 
 from config import app, db, api
 from models import User
@@ -16,28 +17,46 @@ class ClearSession(Resource):
         return {}, 204
 
 class Signup(Resource):
-    
     def post(self):
         json = request.get_json()
         user = User(
-            username=json['username']
+            username=json['username'],
+            password_hash=generate_password_hash(json['password'])
         )
-        user.password_hash = json['password']
         db.session.add(user)
         db.session.commit()
-        return user.to_dict(), 201
+        session['user_id'] = user.id
+        return jsonify(user.to_dict()), 201
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.get(user_id)
+            return jsonify(user.to_dict()), 200
+        else:
+            return {}, 204
 
 class Login(Resource):
-    pass
+    def post(self):
+        json = request.get_json()
+        user = User.query.filter_by(username=json['username']).first()
+        if user and check_password_hash(user.password_hash, json['password']):
+            session['user_id'] = user.id
+            return jsonify(user.to_dict()), 200
+        else:
+            return {'error': 'Invalid username or password'}, 401
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session.pop('user_id', None)
+        return {}, 204
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/config.py
+++ server/config.py
@@ -1,8 +1,8 @@
-from flask import Flask
-from flask_bcrypt import Bcrypt
-from flask_migrate import Migrate
-from flask_restful import Api
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask #type:ignore
+from flask_bcrypt import Bcrypt #type:ignore
+from flask_migrate import Migrate #type:ignore
+from flask_restful import Api #type:ignore
+from flask_sqlalchemy import SQLAlchemy #type:ignore
 
 app = Flask(__name__)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
